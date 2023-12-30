4d283ed9d1431d6ed295ef1903cb619e627612dc
Dmitriy <dsj20477@gmail.com>
2023-12-30 18:09:19 +0300

    add game board

---
+++ src/models/Cell.ts
@@ -0,0 +1,26 @@
+import {Colors} from "./Colors";
+import {Board} from "./Boards";
+
+class Figure {
+}
+
+export class Cell{
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean;
+    id: number;
+
+    constructor(board: Board, x:number, y:number, color:Colors,figure:Figure | null) {
+        this.board = board;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.available = false;
+        this.id = Math.random();
+
+    }
+}
